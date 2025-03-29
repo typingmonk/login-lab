e4a6199e82de4a6ceae3bbe9ac3102cea9196cf9
typingmonk <tmonk.hsieh@tmonk.tw>
2025-03-29 09:31:54 +0800

    Initiate navigator.credentials.create()

--- static/js/webAuthn.js
+++ static/js/webAuthn.js
@@ -13,6 +13,31 @@ function registerWebAuthn(event) {
     )
 }
 
-function createWebAuthnCredential(publicKeyOptions) {
-  console.log(publicKeyOptions);
+async function createWebAuthnCredential(publicKeyOptions) {
+  publicKeyOptions.challenge = base64toArrayBuffer(publicKeyOptions.challenge);
+  publicKeyOptions.user.id = base64toArrayBuffer(publicKeyOptions.user.id);
+
+  // Create credentials using WebAuthn API
+  const credential = await navigator.credentials.create({
+    publicKey: publicKeyOptions
+  });
+
+  // Successful creation - no immediate server communication as requested
+  return credential;
+}
+
+function base64toArrayBuffer(base64Url) {
+  //base64Url to base64
+  let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  const padding = '='.repeat((4 - base64.length % 4) % 4);
+  base64 = base64 + padding;
+
+  //convert to ArrayBuffter
+  const binaryString = window.atob(base64);
+  const arr = new Uint8Array(binaryString.length);
+  for (let i = 0; i < binaryString.length; i++) {
+    arr[i] = binaryString.charCodeAt(i);
+  }
+
+  return arr;
 }
